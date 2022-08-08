13a64c29ba455534fa6bb441cdff85fad2b95c97
tdanelutti@amr.org.ar <tomasdanelutti1@hotmail.com>
2022-08-08 19:12:27 -0300

    uso state para ver si edito o modifico un alumno

--- src/app/pages/administracion/usuarios/alumnos/crear-modificar-alumno/crear-modificar-alumno.page.ts
+++ src/app/pages/administracion/usuarios/alumnos/crear-modificar-alumno/crear-modificar-alumno.page.ts
@@ -5,6 +5,9 @@ import {MessagesService} from '../../../../../services/messages.service';
 import {UsuarioService} from '../../../../../services/usuario.service';
 import {Programa} from '../../../../../model/Programa';
 import {Usuario} from '../../../../../model/Usuario';
+import {Select, Store} from "@ngxs/store";
+import {ResetUsuario, UsuarioState} from "../../../../../state/states/usuario.state";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-crear-modificar-alumno',
@@ -12,17 +15,20 @@ import {Usuario} from '../../../../../model/Usuario';
   styleUrls: ['./crear-modificar-alumno.page.scss'],
 })
 export class CrearModificarAlumnoPage implements OnInit {
+  @Select(UsuarioState.getUsuario) usuarioState: Observable<Usuario>
   formulario: FormGroup;
-  alumno: Usuario;
+  cardHeader: string;
 
   constructor(
       private router: Router,
       private formBuilder: FormBuilder,
       private messagesService: MessagesService,
-      private usuarioService: UsuarioService) { }
+      private usuarioService: UsuarioService,
+      private store: Store) { }
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
+      idUsuario: [],
       dni: [],
       fechaNacimiento: [],
       nombre: [],
@@ -35,13 +41,15 @@ export class CrearModificarAlumnoPage implements OnInit {
       email: [],
       repetirEmail: [],
     });
-  }
-
-  async ionViewWillEnter() {
-    // this.alumno = (await this.storage.get('alumno')) || new Usuario() && this.formulario.reset();
-    if (this.alumno) {
-      this.formulario.patchValue(this.alumno);
-    }
+    this.usuarioState.subscribe(usuario => {
+      if (usuario) {
+        this.formulario.patchValue(usuario);
+      }
+      else {
+        this.formulario.reset();
+      }
+      this.cardHeader = this.formulario.value.idUsuario ? 'Modificar alumno' : 'Crear alumno';
+    });
   }
 
   volver() {
@@ -51,7 +59,8 @@ export class CrearModificarAlumnoPage implements OnInit {
   guardarAlumno() {
     if (this.formulario.valid) {
       this.usuarioService.guardarAlumno(this.formulario.value).subscribe(rta => {
-        this.messagesService.showMessage('Exito', 'Alumno guardado correctamente', 5000);
+        const estado: string = this.formulario.value.idUsuario ? 'modificado' : 'creado';
+        this.messagesService.showMessage('Éxito', `Alumno ${this.formulario.value.nombre} ${estado}`, 5000);
         this.router.navigate(['administrar/alumnos'], {replaceUrl: true});
       }, error => {
         this.messagesService.showMessage('Atención', 'No se pudo guardar el Alumno', 5000);
@@ -63,7 +72,7 @@ export class CrearModificarAlumnoPage implements OnInit {
 
   ionViewDidLeave() {
     this.formulario.reset();
-    // this.storage.remove('alumno');
+    this.store.dispatch(new ResetUsuario());
   }
 
   setearFecha($event: string) {
