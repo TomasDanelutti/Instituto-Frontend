930d21a8f6719e3bf69bdcfe575f6fc3fd01dd45
tdanelutti@amr.org.ar <tomasdanelutti1@hotmail.com>
2022-08-08 17:42:40 -0300

    agregamos dependencias que faltaban

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,26 @@ import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
 import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {states} from './state/app';
 import {CustomComponentsModule} from "./modules/custom-components/custom-components.module";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { AppHttpInterceptor } from './app.httpInterceptor';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     NgxsModule.forRoot(states),
     NgxsStoragePluginModule.forRoot(),
-    NgxsLoggerPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot({disabled: true}),
     NgxsReduxDevtoolsPluginModule.forRoot(),
     CustomComponentsModule,
     HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true},
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
