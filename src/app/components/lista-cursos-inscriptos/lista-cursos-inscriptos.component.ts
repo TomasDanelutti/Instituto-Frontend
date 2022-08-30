6e8f9fbdd76b69039c0b43a3217a78d4430ee6d4
tdanelutti@amr.org.ar <tomasdanelutti1@hotmail.com>
2022-08-30 17:42:24 -0300

    cambio nombre al componente

--- src/app/components/lista-cursos-inscriptos/lista-cursos-inscriptos.component.ts
+++ src/app/components/lista-cursos-inscriptos/lista-cursos-inscriptos.component.ts
@@ -10,47 +10,30 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-vista-cursos',
-  templateUrl: './vista-cursos.component.html',
-  styleUrls: ['./vista-cursos.component.scss'],
+  selector: 'app-lista-cursos-inscriptos',
+  templateUrl: './lista-cursos-inscriptos.component.html',
+  styleUrls: ['./lista-cursos-inscriptos.component.scss'],
 })
-export class VistaCursosComponent implements OnInit {
+export class ListaCursosInscriptosComponent implements OnInit {
   @Select(UsuarioLogueadoState.getUsuarioLogueado) usuarioLogueado: Observable<Usuario>;
   @Input() cursos: Curso[];
-  @Input() insc = true;
-  @Input() home = false;
   usuario: Usuario;
-  inscripcion: FormGroup;
-  constructor(private messagesService: MessagesService,
-              private store: Store,
-              private inscripcionService: InscripcionService,
-              private formBuilder: FormBuilder,
-              private router: Router) { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.usuarioLogueado.subscribe(value => this.usuario = value);
-    this.inscripcion = this.formBuilder.group({
-      idInscripcion: [],
-      usuario: [],
-      curso: [],
-    });
   }
 
-  inscribirse(curso: Curso) {
-    this.inscripcion.controls.usuario.setValue(this.usuario);
-    this.inscripcion.controls.curso.setValue(curso);
-    this.messagesService.showAlert('Atención', `¿Desea inscribirse al curso?`, () => {
-      this.inscripcionService.inscribirse(this.inscripcion.value).subscribe(value => {
-        this.messagesService.showMessage('Éxito', 'Inscripcion realizada con exito', 5000);
-        this.store.dispatch(new SetCursosInscriptos(this.usuario.idUsuario));
-        this.store.dispatch(new SetCursosNoInscriptos(this.usuario.idUsuario));
-      }, error => {
-        this.messagesService.showMessage('Atención', 'No se pudo inscribir al curso', 5000);
-      });
-    });
+  verAlumnos(curso: Curso) {
+
   }
 
-  masInformacion() {
-    this.router.navigate(['/cursos'], { replaceUrl: true });
+  verProfesor(curso: Curso) {
+
+  }
+
+  aulaVirtual(curso: Curso) {
+
   }
 }
--- src/app/pages/lista-cursos/lista-cursos.module.ts
+++ src/app/pages/lista-cursos/lista-cursos.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-import { CursosPageRoutingModule } from './cursos-routing.module';
-
-import { CursosPage } from './cursos.page';
 import {CustomComponentsModule} from '../../modules/custom-components/custom-components.module';
 import {PrimeNgModule} from '../../modules/primeng/primeng.module';
 import {RatingModule} from 'primeng/rating';
+import {ListaCursosPage} from "./lista-cursos.page";
+import {ListaCursosRoutingModule} from "./lista-cursos-routing.module";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
-        CursosPageRoutingModule,
+        ListaCursosRoutingModule,
         CustomComponentsModule,
         PrimeNgModule,
         RatingModule
     ],
-  declarations: [CursosPage]
+  declarations: [ListaCursosPage]
 })
-export class CursosPageModule {}
+export class ListaCursosPageModule {}
