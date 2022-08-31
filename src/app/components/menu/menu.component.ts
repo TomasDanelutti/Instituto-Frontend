77b3151dbb672cb26fe893fe25ed56f4344adca4
tdanelutti@amr.org.ar <tomasdanelutti1@hotmail.com>
2022-08-30 21:33:24 -0300

    funciona el nuevo menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,115 +1,165 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Router} from '@angular/router';
 import {MenuItem} from 'primeng/api';
 import {Select, Store} from '@ngxs/store';
 import {ResetUsuarioLogueado, UsuarioLogueadoState} from '../../state/states/usuarioLogueado.state';
 import {Observable} from 'rxjs';
 import {Rol} from '../../model/rol';
+import {Platform} from "@ionic/angular";
+import {StatusBar} from "@ionic-native/status-bar/ngx";
+import {SplashScreen} from "@ionic-native/splash-screen/ngx";
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnChanges {
     @Select(UsuarioLogueadoState.getRol) rol: Observable<Rol>;
-    rutasMenu: MenuItem[];
     rolSeleccionado: Rol;
-    constructor(private router: Router,
-                private store: Store) {
+    navigate: any;
+
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        private router: Router,
+        private store: Store
+    ) {
+        this.initializeApp();
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.rol.currentValue) {
+            this.armarMenu();
+        }
     }
 
     ngOnInit() {
-        this.rol.subscribe(value => this.rolSeleccionado = value);
+        this.rol.subscribe(value => {
+            this.rolSeleccionado = value;
+            this.armarMenu();
+        });
+    }
+
+    armarMenu() {
         switch (this.rolSeleccionado.idRol) {
             case 0:
-                this.rutasMenu = [
-                    {label: 'Home', icon: 'pi pi-home', routerLink: '/home'},
-                    {label: 'Mi perfil', icon: 'pi pi-book', routerLink: '/mi-perfil'},
-                    {label: 'Cursos disponibles', icon: 'pi pi-book', routerLink: '/lista-cursos'},
-                    {label: 'Salir', icon: 'pi pi-sign-out', command: () => {
-                            this.store.dispatch(new ResetUsuarioLogueado());
-                            this.router.navigate(['/login'], {replaceUrl: true});
-                        } },
-                ];
+                this.navigate =
+                    [
+                        {
+                            title : 'Home',
+                            url   : '/home',
+                            icon  : 'home'
+                        },
+                        {
+                            title: 'Cursos disponibles',
+                            url: '/lista-cursos',
+                            icon: 'book',
+                        },
+                        {
+                            title : 'Mis cursos',
+                            url   : '/lista-cursos',
+                            icon  : 'book'
+                        },
+                        {
+                            title : 'Salir',
+                            url   : '/login',
+                            icon  : 'exit'
+                        },
+                    ];
                 break;
             case 1:
-                this.rutasMenu = [
-                    {label: 'Home', icon: 'pi pi-home', routerLink: '/home'},
-                    {label: 'Mi perfil', icon: 'pi pi-book', routerLink: '/mi-perfil'},
-                    {
-                        label: 'Administrar',
-                        icon: 'pi pi-cog',
-                        items: [{
-                            label: 'cursos',
-                            icon: 'pi pi-plus',
-                            routerLink: '/administrar/cursos'
+                this.navigate =
+                    [
+                        {
+                            title : 'Home',
+                            url   : '/home',
+                            icon  : 'home'
                         },
-                            {
-                                label: 'Programas',
-                                icon: 'pi pi-plus',
-                                routerLink: '/administrar/programas'
-                            },
-                            {
-                                label: 'Usuarios',
-                                icon: 'pi pi-plus',
-                                items: [{
-                                    label: 'Administrativos',
-                                    icon: 'pi pi-plus',
-                                    routerLink: '/administrar/administrativos'
-                                },
-                                    {
-                                        label: 'Alumnos',
-                                        icon: 'pi pi-plus',
-                                        routerLink: '/administrar/alumnos'
-                                    }]
-                            }]
-                    },
-                    {label: 'Salir', icon: 'pi pi-sign-out', command: () => {
-                            this.store.dispatch(new ResetUsuarioLogueado());
-                            this.router.navigate(['/login'], {replaceUrl: true});
-                        } },
-                ];
+                        {
+                            title: 'Cursos disponibles',
+                            url: '/lista-cursos',
+                            icon: 'book',
+                        },
+                        {
+                            title : 'Mis cursos',
+                            url   : '/lista-cursos',
+                            icon  : 'book'
+                        },
+                        {
+                            title : 'Administrar programas',
+                            url   : '/administrar/programas',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Administrar cursos',
+                            url   : '/administrar/cursos',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Administrar alumnos',
+                            url   : '/administrar/alumnos',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Salir',
+                            url   : '/login',
+                            icon  : 'exit'
+                        },
+                    ];
                 break;
             case 2:
-                this.rutasMenu = [
-                    {label: 'Home', icon: 'pi pi-home', routerLink: '/home'},
-                    {label: 'Mi perfil', icon: 'pi pi-book', routerLink: '/mi-perfil'},
-                    {label: 'Cursos disponibles', icon: 'pi pi-book', routerLink: '/lista-cursos'},
-                    {
-                        label: 'Administrar',
-                        icon: 'pi pi-cog',
-                        items: [{
-                            label: 'cursos',
-                            icon: 'pi pi-plus',
-                            routerLink: '/administrar/cursos'
+                this.navigate =
+                    [
+                        {
+                            title : 'Home',
+                            url   : '/home',
+                            icon  : 'home'
                         },
-                            {
-                                label: 'Programas',
-                                icon: 'pi pi-plus',
-                                routerLink: '/administrar/programas'
-                            },
-                            {
-                                label: 'Usuarios',
-                                icon: 'pi pi-plus',
-                                items: [{
-                                    label: 'Administrativos',
-                                    icon: 'pi pi-plus',
-                                    routerLink: '/administrar/administrativos'
-                                },
-                                    {
-                                        label: 'Alumnos',
-                                        icon: 'pi pi-plus',
-                                        routerLink: '/administrar/alumnos'
-                                    }]
-                            }]
-                    },
-                    {label: 'Salir', icon: 'pi pi-sign-out', command: () => {
-                            this.store.dispatch(new ResetUsuarioLogueado());
-                            this.router.navigate(['/login'], {replaceUrl: true});
-                        } },
-                ];
+                        {
+                            title: 'Cursos disponibles',
+                            url: '/lista-cursos',
+                            icon: 'book',
+                        },
+                        {
+                            title : 'Mis cursos',
+                            url   : '/lista-cursos',
+                            icon  : 'book'
+                        },
+                        {
+                            title : 'Administrar programas',
+                            url   : '/administrar/programas',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Administrar cursos',
+                            url   : '/administrar/cursos',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Administrar alumnos',
+                            url   : '/administrar/alumnos',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Administrar administrativos',
+                            url   : '/administrar/administrativos',
+                            icon  : 'build'
+                        },
+                        {
+                            title : 'Salir',
+                            url   : '/login',
+                            icon  : 'exit'
+                        },
+                    ];
                 break;
         }
     }
+
+    initializeApp() {
+        this.platform.ready().then(() => {
+        });
+    }
+
 }
