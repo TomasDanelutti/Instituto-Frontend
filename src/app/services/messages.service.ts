ccd8dae678b160abf8a5494d96f1dc56886a44eb
tdanelutti@amr.org.ar <tomasdanelutti1@hotmail.com>
2022-09-06 15:13:40 -0300

    nuevos mensajes

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,5 +1,7 @@
-import {Injectable} from '@angular/core';
-import {AlertController, ToastController} from '@ionic/angular';
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import Swal, { SweetAlertOptions, SweetAlertResult } from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -8,20 +10,22 @@ export class MessagesService {
 
   toast: HTMLIonToastElement;
 
-  constructor(private toastController: ToastController, private alertController: AlertController) {
-  }
+  constructor(private toastController: ToastController, private alertController: AlertController) { }
 
+
+  // IONIC TOASTS
   async showMessage(header: string, message: string, duration: number) {
 
-    if (this.toast) {
+    if(this.toast) {
       // Si ya existe un toast lo cierro, para mostrar el nuevo.
       this.toast.dismiss();
     }
 
+    // Guardo el toast.
     this.toast = await this.toastController.create({
-      header,
-      message,
-      duration,
+      header: header,
+      message: message,
+      duration: duration,
       buttons: [
         {
           text: 'Cerrar',
@@ -33,20 +37,17 @@ export class MessagesService {
     this.toast.present();
   }
 
-  async showAlert(header: string, message: string,
-                  handlerAceptar: any, textoAceptar = 'Aceptar', textoCancelar = 'Cancelar',
-                  handlerCancelar?: any) {
+  async showtAlert(header: string, message: string, handler: any) {
 
     const alert = await this.alertController.create({
-      header,
-      message,
+      header: header,
+      message: message,
       buttons: [
         {
-          text: textoAceptar,
-          handler: handlerAceptar,
-        }, {
-          text: textoCancelar,
-          handler: handlerCancelar
+          text: 'Aceptar',
+          handler: handler,
+        },{
+          text: 'Cancelar',
         }
       ]
     });
@@ -54,4 +55,148 @@ export class MessagesService {
     await alert.present();
   }
 
+  //  SWEET ALERT
+  ventanaExitosa(titulo: string, textoCuerpo?: string) {
+    return Swal.fire({
+      icon: 'success',
+      title: titulo,
+      text: textoCuerpo,
+      showConfirmButton: true,
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#1262e0',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
+
+  ventanaInfo(titulo: string, textoCuerpo?: string) {
+    Swal.fire({
+      icon: 'info',
+      title: titulo,
+      text: textoCuerpo,
+      showConfirmButton: true,
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#1262e0',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
+
+  ventanaError(titulo: string, textoCuerpo?: string) {
+    Swal.fire({
+      icon: 'error',
+      title: titulo,
+      text: textoCuerpo,
+      showConfirmButton: true,
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#1262e0',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
+
+  ventanaWarning(titulo: string, textoCuerpo?: string) {
+    Swal.fire({
+      icon: 'warning',
+      title: titulo,
+      text: textoCuerpo,
+      showConfirmButton: true,
+      confirmButtonText: 'Aceptar',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
+
+  ventanaConfirmar(titulo: string, textoCuerpo: string): Promise<SweetAlertResult<any>> {
+    return Swal.fire({
+      title: titulo,
+      text: textoCuerpo,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#1262e0',
+      cancelButtonColor: '#90a4ae',
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
+
+  ventanaErrorConFooter(titulo: string, textoCuerpo?: string) {
+    Swal.fire({
+      icon: 'error',
+      title: titulo,
+      text: textoCuerpo,
+      showConfirmButton: true,
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#1262e0',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      footer: 'Por favor, contáctese con nosotros.'
+    });
+  }
+
+  ventanaCrearCuenta(titulo: string, textoCuerpo: string): Promise<SweetAlertResult<any>> {
+    return Swal.fire({
+      title: titulo,
+      text: textoCuerpo,
+      icon: 'warning',
+      showCloseButton: true,
+      showDenyButton: true,
+      confirmButtonText: 'Médico',
+      confirmButtonColor: '#1262e0',
+      denyButtonText: `Institución`,
+      denyButtonColor: '#90a4ae',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
+
+  ventanaErrorCustomTextConfirmButton(titulo: string, textoCuerpo: string, textoBoton: string, mostrarBotonCancelar: boolean = false) {
+
+    let swalFire: SweetAlertOptions<any, any> = {
+      icon: 'error',
+      title: titulo,
+      text: textoCuerpo,
+      showConfirmButton: true,
+      confirmButtonText: textoBoton,
+      confirmButtonColor: '#1262e0',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    };
+
+
+    if (mostrarBotonCancelar) {
+      swalFire.showCancelButton = true;
+      swalFire.cancelButtonColor = '#90a4ae';
+      swalFire.cancelButtonText = 'Cancelar';
+    }
+
+    return Swal.fire(swalFire);
+  }
+
+  ventanaWarningCustomButtons(titulo: string, textoCuerpo: string, textoBotonConfirmar: string, textoBotonCancelar: string) {
+    return Swal.fire({
+      icon: 'warning',
+      title: titulo,
+      text: textoCuerpo,
+      showCancelButton: true,
+      cancelButtonColor: '#90a4ae',
+      cancelButtonText: textoBotonCancelar,
+      showConfirmButton: true,
+      confirmButtonText: textoBotonConfirmar,
+      confirmButtonColor: '#1262e0',
+      heightAuto: false,
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    });
+  }
 }
